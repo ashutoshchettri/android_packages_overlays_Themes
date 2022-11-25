7bed7ae43b9c6b8d260e71f0a20a5752315659e0
El Dainosor <eldainosor@gmail.com>
2022-11-25 23:08:41 +0100

    Overlay: Import UIStyles from Bootleggers
    * Taken from https://github.com/Bootleggers-BrokenLab/vendor_shishufied/commits/tirimbino/uistyles
    
    Change-Id: I3477813906c5ce37df87bf9b170cc21d0fe1300f
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- themes.mk
+++ themes.mk
@@ -466,6 +466,39 @@ PRODUCT_PACKAGES += \
     LornDataOverlay \
     PlumpyDataOverlay
     
+# Themes
+PRODUCT_PACKAGES += \
+    AndroidBlackThemeOverlay \
+    UnmonetThemeAndroidOverlay \
+    UnmonetThemeSettingsOverlay \
+    NexodusThemeAndroidOverlay \
+    NexodusThemeSettingsOverlay \
+    NexodusThemeSystemUIOverlay \
+    ProjectOptronicThemeAndroidOverlay \
+    ProjectOptronicThemeSettingsOverlay \
+    ProjectOptronicThemeSystemUIOverlay \
+    PacleggersThemeAndroidOverlay \
+    PacleggersThemeSettingsOverlay \
+    PacleggersThemeSystemUIOverlay \
+    SolarizedThemeAndroidOverlay \
+    SolarizedThemeSettingsOverlay \
+    SolarizedThemeSystemUIOverlay \
+    ShishuIllusionsThemeAndroidOverlay \
+    ShishuIllusionsThemeSettingsOverlay \
+    ShishuIllusionsThemeSystemUIOverlay \
+    ShishuImmensityThemeAndroidOverlay \
+    ShishuImmensityThemeSettingsOverlay \
+    ShishuImmensityThemeSystemUIOverlay \
+    ShishuThemeAndroidOverlay \
+    ShishuThemeSettingsOverlay \
+    ShishuThemeSystemUIOverlay \
+    ShishuNightsThemeAndroidOverlay \
+    ShishuNightsThemeSettingsOverlay \
+    ShishuNightsThemeSystemUIOverlay \
+    ShishuAmalgamationThemeAndroidOverlay \
+    ShishuAmalgamationThemeSettingsOverlay \
+    ShishuAmalgamationThemeSystemUIOverlay
+
 # Icon Data
 PRODUCT_PACKAGES += \
     IconDataDefault \
